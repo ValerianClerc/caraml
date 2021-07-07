8cd2a96c9f3e2108c836a830d8e5ac3497cabcd2
Valerian Clerc <valerian.clerc@gmail.com>
2021-07-07 13:17:12 -0400

    Lexer: Implemented `lexAlpha`, added more symbol cases

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Lexer where
 
 import           Data.Char
+import           Data.List
 
 data Token =
   LParen
@@ -42,30 +45,66 @@ runLex [] = [EOF]
 runLex input@(x:xs)
   | isSpace x = runLex xs
   | isDigit x = lexDigit input
-  | isAlphaNum x = lexAlphaNum input
+  | isAlpha x = lexAlpha input
   | otherwise = case x of
       '(' -> parseParens input    -- check for comments with (*
       ')' -> RPAREN : runLex xs
+      '"' -> parseString input
+      '\'' -> parseChar input
       '-' -> MINUS : runLex xs
       '+' -> PLUS : runLex xs
       '*' -> ASTERISK : runLex xs
       '/' -> DIVIDE : runLex xs
       '=' -> EQU : runLex xs
-      '>' -> GRT : runLex xs
-      '<' -> LST : runLex xs
+      '>' -> if null xs
+        then  GRT : [EOF]
+        else let y:ys = xs in
+        case y of
+          '=' -> GEQ : runLex ys
+          _   -> GRT : runLex xs
+      '<' -> if null xs then LST : [EOF]
+        else let y:ys = xs in
+        case y of
+          '=' -> LEQ : runLex ys
+          '>' -> NEQ : runLex ys
+          _   -> LST : runLex xs
       ',' -> COMMA : runLex xs
       '_' -> UNDERSCORE : runLex xs
       ';' -> COMMA : runLex xs
       _   -> error $ "Invalid character: " ++ [x]
-      -- TODO: <=, <>, >=
 
 -- (x1s, x2s)= span isAlpha input
 
 parseParens :: String -> [Token]
 parseParens = undefined
 
-lexAlphaNum :: String -> [Token]
-lexAlphaNum = undefined
+parseString :: String -> [Token]
+parseString = undefined
+
+parseChar :: String -> [Token]
+parseChar = undefined
+
+lexAlpha :: String -> [Token]
+lexAlpha [] = [EOF]
+lexAlpha s@(x:_)
+  | isAlpha x = matchPrefix ident : runLex rest
+  | otherwise = error "LexAlpha: Input is not alphabetical, this should never be reached"
+  where
+    (ident, rest) = span isAlphaNum s
+
+    matchPrefix :: String -> Token
+    matchPrefix str
+      | str == "not" = NOT
+      | str == "if" = IF
+      | str == "then" = THEN
+      | str == "else" = ELSE
+      | str == "let" = LET
+      | str == "in" = IN
+      | str == "rec" = REC
+      | str == "let" = LET
+      | str == "true" = BOOLEAN True
+      | str == "false" = BOOLEAN False
+      | otherwise = IDENT str
 
 lexDigit :: String -> [Token]
 lexDigit [] = [EOF]
