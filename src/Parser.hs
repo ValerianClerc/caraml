fe968b7d6688765db5bf0070cfaaea53b5c16a2e
Valerian Clerc <valerian.clerc@gmail.com>
2021-07-19 19:59:46 -0400

    Parser: added literals + let/in exprs

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,20 +1,44 @@
 module Parser where
 
+import           Data.List.Split
 import           Lexer
 
 
 data Expr =
-  Literal
+  LInt Int | LChar Char | LBool Bool | LString String
   | BinOp Expr Op Expr
   | Let {letVar :: String, letEqual :: Expr, letIn :: Expr}
   | Conditional {condBool:: Expr, condIf:: Expr, condElse :: Expr}
   | VarExpr {varExprName :: String}
   | FunDecl {funDeclName :: String, funDeclArgs :: [String], funDeclExpr :: Expr}
   | FunCall {funCallName :: String, funCallArgs :: [Expr]}
+  deriving (Show, Eq)
 
-data Literal = Int | Char | Bool
 data Op = OpPlus | OpMinus | OpMult | OpDiv | OpEq | OpNeq | OpLt | OpGt | OpLeq | OpGeq deriving (Show, Eq)
 
-
 runParser :: [Token] -> [Expr]
-runParser = undefined
+runParser [] = []
+runParser (SC:xs) = runParser xs
+runParser input =
+  let exprList = splitOn [SC] input in
+    map (\x -> let (x', _) = parseExpr x in x') exprList
+
+-- TODO: remove left recursion from grammar
+
+parseExpr :: [Token] -> (Expr, [Token])
+parseExpr []    = error "parseExpr was called with an empty token list"
+parseExpr ((DIGIT i):xs) = (LInt i, xs)
+parseExpr ((CHAR c):xs) = (LChar c, xs)
+parseExpr ((STRING s):xs) = (LString s, xs)
+parseExpr ((BOOLEAN b):xs) = (LBool b, xs)
+parseExpr ((IDENT s):xs) = (VarExpr {varExprName = s}, xs)
+parseExpr (LET:(IDENT s):EQU:xs) =
+  if head rest == IN then
+    let
+      (inExpr, rest') = parseExpr $ tail rest
+      in
+      (Let {letVar = s, letEqual = equalExpr, letIn = inExpr}, rest')
+  else error "expected \"in\" after let expression"
+  where
+    (equalExpr, rest) = parseExpr xs
+parseExpr input = error $ "Unimplemented parser case: " ++ show input
