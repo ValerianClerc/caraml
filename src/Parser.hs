ee512b95b7304fa753d5c64126d5d7e929c3e714
Valerian Clerc <valerian.clerc@gmail.com>
2021-07-18 21:27:27 -0400

    Parser: init AST types

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,7 +2,19 @@ module Parser where
 
 import           Lexer
 
-data AST = Conditional | VarDecl deriving (Show, Eq)
 
-runParser :: [Token] -> AST
+data Expr =
+  Literal
+  | BinOp Expr Op Expr
+  | Let {letVar :: String, letEqual :: Expr, letIn :: Expr}
+  | Conditional {condBool:: Expr, condIf:: Expr, condElse :: Expr}
+  | VarExpr {varExprName :: String}
+  | FunDecl {funDeclName :: String, funDeclArgs :: [String], funDeclExpr :: Expr}
+  | FunCall {funCallName :: String, funCallArgs :: [Expr]}
+
+data Literal = Int | Char | Bool
+data Op = OpPlus | OpMinus | OpMult | OpDiv | OpEq | OpNeq | OpLt | OpGt | OpLeq | OpGeq deriving (Show, Eq)
+
+
+runParser :: [Token] -> [Expr]
 runParser = undefined
